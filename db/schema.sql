d07e56d67f340fbca084d0056727b4f2d660fd7c
Erin Giannette <emg4784@yahoo.com>
2016-11-07 16:43:42 -0600

    adding sql

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(25) NOT NULL,
+	devoured BOOLEAN NOT NULL DEFAULT 0,
+	ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+)
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('double bacon');
+
+INSERT INTO burgers (burger_name) VALUES ('cheese burger');
+
+INSERT INTO burgers (burger_name) VALUES ('steak burger');
